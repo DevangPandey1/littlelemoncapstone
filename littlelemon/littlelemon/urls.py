45bf77d45244c426fed214e103d5c2f3246ae0ca
DevangPandey1 <d6vangpandey@gmail.com>
2023-12-23 22:48:17 -0700

    Multicommit

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin 
-from django.urls import path, include  
- 
-urlpatterns = [ 
-    path('admin/', admin.site.urls), 
-    path('restaurant/', include('restaurant.urls')),
+from django.contrib import admin
+from django.db import router
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api/', include("restaurant.urls")),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
